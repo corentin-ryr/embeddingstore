f310534e1090da9ef6341f40320fa90b2146c327
Royer Corentin <royc@elcaNet.local>
2023-07-19 16:07:04 +0200

    Add dependency

--- setup.py
+++ setup.py
@@ -19,4 +19,8 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.6",
+    install_requires=[
+        "conda-faiss-cpu"
+        # Add other dependencies as needed
+    ],
 )
